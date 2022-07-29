295240b40bafa93047ab6c31e7cf1a1786d742fb
kevinddenny <cb009204@students.apiit.lk>
2022-07-29 10:53:12 +0000

    c

--- dist/js/622.f5fb4157.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkcey"]=self["webpackChunkcey"]||[]).push([[622],{8622:function(e,n,r){r.r(n),r.d(n,{default:function(){return l}});var t=r(3396);const o={class:"offer"},c=(0,t._)("h1",null,"This is an about page",-1);function s(e,n,r,s,u,a){const f=(0,t.up)("Footer");return(0,t.wg)(),(0,t.iD)("div",o,[c,(0,t.Wm)(f)])}var u=r(8949),a={name:"OfferView",components:{Footer:u.Z}},f=r(89);const i=(0,f.Z)(a,[["render",s]]);var l=i}}]);
-//# sourceMappingURL=622.f5fb4157.js.map

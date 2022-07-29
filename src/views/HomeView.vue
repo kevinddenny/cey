adf9f960fcaaec89161cb4f787ef38b8b8acab5e
kevinddenny <cb009204@students.apiit.lk>
2022-07-29 11:42:14 +0000

    c

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,145 +1,68 @@
 <template>
-  <div class="heritage">
-  <div class="headerImg">
-      <img class="mainImg" src="Images/Heritage.jpg" alt="Heritages">
+  <div class="home">
+      
+      <div class="headerImg">
+      <img
+        class="homeImg"
+        src="Images/HomePage.jpg"
+        alt="HomePage"
+        width="100%"
+      />
       <div class="headerImgText">
-        <h1>Heritages</h1>
+        <h1 class="imgh1">Ayubowan!</h1>
+        <h2 class="imgh2">Come See Lanka...</h2>
       </div>
     </div>
 
-    <div>
-      <!-- Proper content needs to be thought of and added-->
-      <p class="mainP">
-        With a 3000 year history, Sri Lanka is home to some of the world's most
-        ancient cities, such as Anuradhapura, Polonnaruwa, and Digamadulla.
-        These cities' once magnificent townships, palaces, temples, monasteries,
-        hospitals, and theaters, all of which were intricately carved and
-        modeled out of stone, have since been abandoned and forgotten amidst the
-        country's towering jungles.<br />
-      </p>
-
-      <p class="mainP">
-        The Kingdom of Anuradhapura is the most renowned and beautiful of all
-        the ancient cities in Lanka. One of the oldest continuously inhabited
-        towns in the world and Sri Lanka's third and longest-serving capital is
-        also one of the most revered by Buddhists worldwide. It served as Sri
-        Lanka's capital from the Fourth Century BC to the start of the eleventh
-        Century and was one of South Asia's most enduring and stable hubs of
-        political authority and urban activity. <br />
-      </p>
-
-      <p class="mainP">
-        Some of the most cutting-edge landscaping features and some of the
-        oldest paintings ever discovered in the nation may be seen at Sigiriya,
-        a fortification and water park built in the fifth century AD.<br />
-      </p>
-
-      <p class="mainP">
-        The Monarchy of Kandy, the final kingdom in Sri Lanka, is proof of the
-        country's people's capacity to emerge from the ashes. The Kandyan
-        Kingdom still has finely carved and constructed homes, palaces, and
-        temples that have been preserved for about 500 years despite having been
-        destroyed and decimated by the invading Portuguese more than three
-        times. <br />
-      </p>
-
-      <div class="row">
-        <div class="column2">
-          <h3>YAPAHUWA CITADEL </h3>
-          <img id="boximg" src="Images/Yapahuwa.webp" alt="Yapahuwa">
-          <p>>
-            The Yapahuwa citadel, one of Sri Lanka's flimsy medieval capital towns,
-            is constructed on a huge granite rock that abruptly rises from the
-            surrounding lowland plain. <br />
-          </p>
-    
-          <p>
-            Yapahuwa, which was the capital city in the second half of the 13th
-            century, was located in Wayamba Province midway between Kurunegala and
-            Anuradhapura. It functioned as a palace for King Buvanekabahu and a
-            military bastion against invasions from the north. A long, steep ornate
-            stairway leads to the Tooth Temple, where the Tooth Relic was carried
-            from Dambadeniya in 1272 before being kidnapped to South India by the
-            Pandyas in 1284. Traces of old structures and combat defenses may be
-            observed around the site.<br />
-          </p>
-        </div>
-        <div class="column2">
-          <h3>TANTIRIMALEE MONASTERY</h3>
-          <img id="boximg" src="Images/Tantirimalee.jpg" alt="Tantirimalee">
-            <p>The ancient monastic community of Tantirimalee dates back to the time
-            before Christ and is surrounded by an amazing sea of granite rock.<br />
-          </p>
-          <p>
-            A lily-strewn tank (ancient reservoir) will soon be reached as you make
-            your way across the rock surface, and if you keep going, descending down
-            into the dry lowland dipterocarp forest, you will ultimately find a
-            complex of caves with rock drawings dating back to the third century BC.
-            The drawings, which are allegedly meant to show pre-historic life with
-            humans and animals all depicted, require a "creative" mind to understand
-            the significance behind them, as even the instructions indicate.
-            However, to the untrained or "uncreative" eye, they mostly look like
-            noughts and crosses.<br />
-          </p>
-        </div>
-        <div class="column2">
-          <h3>SIGIRIYA ROCK FORTRESS</h3>
-          <img id="boximg" src="Images/sigiriya.jfif" alt="Sigiriya">
-            <p>The huge rock column that rises approximately 200 meters high at the old
-            rock castle known as Sigiriya Lion Rock is its most distinctive feature.
-            The location was chosen as the new capital by King Kasyapa during his
-            reign (477-495 AD). He painted murals on the walls and constructed a
-            magnificent castle directly atop the granite column. The palace was only
-            accessible by a huge carved lion's mouth. It is situated at the top of a
-            granite mountain called "Lion's Rock," which towers above the forest
-            from all sides and is 180 meters high. Access to the location is made
-            possible via a system of galleries and staircases that emerge from the
-            mouth of a massive lion made of brick and plaster. <br /></p>
-        </div>
+    <h1 id="mainHeader">An island escape awaits you</h1>
+    <!--flex boxes needed here three rows three coloumns-->
+    <div class="rows">
+      <div class="columns">
+        <img src="Images/WildLife.jpg" alt="Wildlife" />
+        <router-link id="gridLink" to="/wild">WILDLIFE</router-link>
       </div>
-
-      <div class="row">
-        <div class="column2">
-          <h3>SINHARAJA RAINFOREST</h3>
-          <img id="boximg" src="Images/sinharaja2.jpg" alt="Sinharaja">
-            <p>Sinharaja, in the southwest of Sri Lanka, is the last remaining piece of
-            primary tropical rainforest in the nation. The trees make up more than
-            60% of the population, and many of them are unusual. There is a lot of
-            endemic wildlife in the reserve, notably birds, but there are also many
-            different kinds of insects, reptiles, and uncommon amphibians, as well
-            as more than half of Sri Lanka's endemic butterfly and mammal species.</p>
-        </div>
-        <div class="column2">
-          <h3>ANCIENT CITY OF POLONNARUWA</h3>
-          <img id="boximg" src="Images/polonnaruwa.jpg" alt="Polonnaruwa">
-          <p>
-          Following the fall of the Anuradhapura Kingdom in 993, the Polonnaruwa Kingdom 
-          or Ancient City of Polonnaruwa served as Sri Lanka's second capital for three centuries, 
-          from the 11th to the 13th century. It is situated in Sri Lanka's north central province. 
-          In 1982, Polonnaruwa, then known as the Ancient City of Polonnaruwa, was designated as a World 
-          Heritage Site by UNESCO because of its importance as an archaeological site and its advanced 
-          technological capabilities.<br /></p>
-        </div>
-        <div class="column2">
-          <h3>ANCIENT CITY OF ANURADHAPURA</h3>
-          <img id="boximg" src="Images/Anuradhapura.jpg" alt="Anuradhapura">
-          <p>Sanghamitta, the founder of an order of Buddhist nuns, transported a cutting 
-            from the "tree of enlightenment," the Buddha's fig tree, to his sacred city in 
-            the third century B.C. Following an invasion in 993, Anuradhapura, the political 
-            and ecclesiastical center of Ceylon for 1,300 years, was abandoned. The magnificent 
-            site, with its palaces, monasteries, and monuments, is once again accessible after 
-            spending many years hidden away in thick vegetation.</p>
-        </div>
+      <div class="columns">
+        <img src="Images/Beaches.jpg" alt="Beaches" />
+        <router-link id="gridLink" to="/beach">BEACHES</router-link>
       </div>
+      <div class="columns">
+        <img src="Images/Heritages.jpg" alt="Heritages" />
+        <router-link id="gridLink" to="/heritage">HERITAGES</router-link>
+      </div>
+      <div class="columns">
+        <img src="Images/Pilgrimage.jpg" alt="Pilgrimage" />
+        <router-link id="gridLink" to="/pilgrim">PILGRIMAGE</router-link>
+      </div>
+    </div>
+    <div class="rows">
+      <div class="columns">
+        <img src="Images/Scenic.jpg" alt="Scenic" />
+        <router-link id="gridLink" to="/scenic">SCENIC</router-link>
+      </div>
+      <div class="columns">
+        <img src="Images/Adventure.jpg" alt="Adventure" />
+        <router-link id="gridLink" to="/adventure">ADVENTURE</router-link>
+      </div>
+      <div class="columns">
+        <img src="Images/Cultural.jpg" alt="Cultural" />
+        <router-link id="gridLink" to="/cultural">CULTURAL</router-link>
+      </div>
+      <div class="columns">
+        <img src="Images/Botanical.jpg" alt="Botanical" />
+        <router-link id="gridLink" to="/botanical">BOTANICAL</router-link>
+
+      </div>
+    </div>
+      
  <Footer></Footer>
-  </div>
+  
   </div>
 </template>
 <script>
 import Footer from '@/components/FooterComp.vue'
 
 export default {
-name: 'HeritageView',
+name: 'HomeView',
 components: {
   Footer,
     }
@@ -147,171 +70,115 @@ components: {
 </script>
 
 <style>
-.headerImg{
-  position: relative;
-  text-align: center;
-  color: black;
-  width: 100%;
-  font-size: 40px;
-  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;;
-  }
-  .headerImgText {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-  }
-
-  .mainImg {
-    object-fit: cover;
-    object-position: 20% 50%;
-    width: 100%;
-    height: 200px;  
-    opacity: 0.5;
-  }
-
-  .mainP {
-    padding-left: 5%;
-    padding-right: 5%;
-    font-family: 'Times New Roman', Times, serif;
-    font-weight: lighter;
-  }
-
-  @media sreen and (mad-width:500px){
-    .header{
-      padding: opx 20px;
-      background-color: purple;
-    }
-  }
-  
-
-  
-p {
-      font-size:16px;
-      line-height:24px;
-      padding: 5px;
-    }
-      
-.container{
-      width:100%;
-      margin:0 auto;
-      display:flex;
-      list-style:none;
-    }
-           
-.row {
+.homeImg {
+  opacity: 0.8;
+}
+.rows {
   display: flex;
   flex-wrap: wrap;
   padding: 0 4px;
 }
 
-
-/* Sea Blue Boxes */
-
-.column1 {
-  flex: 29%;
-  max-width: 29%;
-  margin: 15px;
-  border: solid 7px #276F8B;
-  background-color: rgb(199, 231, 242);
-  border-radius: 5px;
+.columns {
+  flex: 25%;
+  max-width: 24%;
+  margin: 5px;
 }
 
-/* Golden Boxes */
-
-.column2 {
-  flex: 29%;
-  max-width: 29%;
-  margin: 15px;
-  border: solid 7px #FAB453;
-  background-color: rgb(255, 255, 222);
-  border-radius: 5px;
-}
-
-/* Green Boxes */
-
-.column3 {
-  flex: 29%;
-  max-width: 29%;
-  margin: 15px;
-  border: solid 7px #357960;
-  background-color: rgb(241, 250, 241);
-  border-radius: 5px;
-}
-
-#boximg{
+.columns img {
   vertical-align: middle;
   width: 100%;
   height: 200px;
   border-radius: 5px;
 }
 
-.card{
-      padding:15px 25px;
-    }
-      
-.columnimg{
-      max-width:100%;  
-    }
-  
 @media screen and (max-width: 768px){
+  .rows {
+    display: flex;
+    flex-wrap: wrap;
+    padding: 0 4px;
+  }
 
-        .container{
-          width:100%;
-          margin:0 auto;
-          display:flex;
-          list-style:none;
-        }
-               
-    .row {
-      display: flex;
-      flex-wrap: wrap;
-      padding: 0 4px;
+  .columns{
+    flex: 100%;
+    max-width: 100%;
+    margin: 5px;
+  }
+}
+
+#gridLink {
+  transition: 0.5s;
+  opacity: 0;
+  position: relative;
+  bottom: 50%;
+  right: inherit;
+  color: black;
+  font-size: xx-large;
+  text-decoration: solid;
+  font-weight: bolder;
+}
+.columns:hover img {
+  opacity: 0.2
+}
+.columns:hover #gridLink {
+  opacity: 1;
+}
+
+#mainHeader {
+  text-align: center;
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  font-size: 50px;
+  font-style: italic;
+}
+
+.offersImg {
+position: relative;
+text-align: center;
+color: white;
+}
+
+.offersText {
+  position: absolute;
+  top: 8px;
+  left: 16px;
+}
+
+.headerImg {
+position: relative;
+text-align: center;
+color: white;
+}
+
+.headerImgText {
+  position: absolute;
+  top: 5%;
+  right: 5%;
+  color: black;
+}
+
+.imgh1 {
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  font-size: 100px;
+  font-style: italic ;
+  text-shadow: 10px;
+}
+
+.imgh2 {
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  font-size: 70px;
+  font-style: italic ;
+  text-shadow: 10px;
+}
+
+@media screen and (max-width: 1000px){
+  .headerImg {
+    display: none;
     }
-    .column1 {
-      flex: 100%;
-      max-width: 100%;
-      margin: 15px;
-      border: solid 7px #276F8B;
-      background-color: rgb(199, 231, 242);
-      border-radius: 5px;
+    
+    .headerImgText {
+      display: none;
     }
-    
-    /* Golden Boxes */
-    
-    .column2 {
-      flex: 100%;
-      max-width: 100%;
-      margin: 15px;
-      border: solid 7px #FAB453;
-      background-color: rgb(255, 255, 222);
-      border-radius: 5px;
-    }
-    
-    /* Green Boxes */
-    
-    .column3 {
-      flex: 100%;
-      max-width: 100%;
-      margin: 15px;
-      border: solid 7px #357960;
-      background-color: rgb(241, 250, 241);
-      border-radius: 5px;
-    }
-    
-    #boximg{
-      vertical-align: middle;
-      width: 100%;
-      height: 200px;
-      border-radius: 5px;
-    }
-    
-    .card{
-          padding:15px 25px;
-        }
-          
-    .columnimg{
-          max-width:100%;  
-        }
-      }
+}
+  
+
 </style>
